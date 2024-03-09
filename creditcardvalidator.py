06b30eb40c212e53dcdcc97d2b1c18a6e557c785
Aletivineeshareddy <aletivineeshareddy43@gmail.com>
2024-03-09 20:30:59 +0530

    credit card code changes

---
+++ creditcardvalidator.py
@@ -0,0 +1,24 @@
+def ok(s):
+    import re
+    if not s[0] in "456": return False
+    if not re.match("[0-9-]+", s): return False
+    if sum([1 if "0" <= _ and _ <= "9" else 0 for _ in s]) != 16: return False
+    if not (len(s) == 16 or len(s) == 19 and s[4] == "-" and s[9] == "-" and s[14] == "-"): return False
+    s = s.replace("-", "")
+    for i in range(len(s)-3):
+        if s[i] == s[i+1] and s[i] == s[i+2] and s[i] == s[i+3]: return False
+    return True
+
+import sys
+
+stdin = sys.stdin
+
+t = int(stdin.readline())
+import re
+for z in range(t):
+    line = stdin.readline().rstrip()
+    if(ok(line)):
+        print("Valid")
+    else:
+        print("Invalid")
+
